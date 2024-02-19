3c5e2a0c5e75164c6f2f8c7021b770b0c945f87b
Jesus <Jesus@DESKTOP-HQQFBH7>
2024-02-18 21:57:22 -0700

    Login terminado?

---
+++ MenuHamburguesa/MenuHamburguesa/ViewModel/VMLogin.cs
@@ -0,0 +1,43 @@
+﻿using AirePuro.View;
+using MenuHamburguesa.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MenuHamburguesa.ViewModel
+{
+    internal class VMLogin : BaseViewModel
+    {
+        public VMLogin(INavigation naivigation)
+        {
+            Navigation = naivigation;
+
+        }
+
+        #region Procesos
+
+        public async Task IniciarSecion()
+        {
+
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        public async Task Registrar()
+        {
+
+            await Navigation.PushAsync(new Registrarte());
+        }
+
+        #endregion
+
+
+        #region Comandos
+
+        public ICommand Iniciarcommand => new Command(async () => await IniciarSecion());
+        public ICommand Registrarcommand => new Command(async () => await Registrar());
+        #endregion
+    }
+}
--- MenuHamburguesa/MenuHamburguesa/Views/Login.xaml.cs
+++ MenuHamburguesa/MenuHamburguesa/Views/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MenuHamburguesa.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace MenuHamburguesa.Views
         public Login()
         {
             InitializeComponent();
+            BindingContext = new VMLogin(Navigation);
         }
     }
 }
