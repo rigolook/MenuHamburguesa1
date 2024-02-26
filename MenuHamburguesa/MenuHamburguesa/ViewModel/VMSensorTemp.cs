e64326d1353872c7eb39e1299647ac7c5484d220
Jesus <Jesus@DESKTOP-HQQFBH7>
2024-02-26 00:54:56 -0700

    Monitoreo agregaddo

---
+++ MenuHamburguesa/MenuHamburguesa/ViewModel/VMSensorTemp.cs
@@ -0,0 +1,62 @@
+﻿using MenuHamburguesa.Models;
+using MenuHamburguesa.Views.Pantallas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MenuHamburguesa.ViewModel
+{
+    public class VMSensorTemp : BaseViewModel
+    {
+        #region variables
+        public List<MSenTemp> _SensoresVentiladores;
+        #endregion
+
+        public VMSensorTemp(INavigation naivigation)
+        {
+            Navigation = naivigation;
+
+
+            ///Esto se quitara despues 
+            _SensoresVentiladores = new List<MSenTemp>
+            {
+                new MSenTemp { Habitacion = "Cocina", Temp =$"Temperatura: {32}°",Humedad=$"Humedad: {30}%" },
+                new MSenTemp { Habitacion = "Cuarto de lavabo", Temp =$"Temperatura: {16}",Humedad=$"Humedad: {30}%"}
+            };
+        }
+
+        #region Procesos
+        public List<MSenTemp> Lista
+        {
+            get { return _SensoresVentiladores; }
+            set
+            {
+                SetValue(ref _SensoresVentiladores, value);
+                OnpropertyChanged();
+
+            }
+        }
+        #endregion
+        #region Procesos
+
+
+
+        public async Task IrSensoresGas()
+        {
+
+            await Navigation.PushAsync(new SenGaz());
+        }
+
+        #endregion
+
+
+        #region Comandos
+
+
+        public ICommand IrSensoresGascommand => new Command(async () => await IrSensoresGas());
+        #endregion
+    }
+}
---
+++ MenuHamburguesa/MenuHamburguesa/Views/Pantallas/Ventiladores.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MenuHamburguesa.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MenuHamburguesa.Views.Pantallas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Ventiladores : ContentPage
+	{
+		public Ventiladores()
+		{
+			InitializeComponent ();
+            //NavigationPage.SetHasBackButton(this, false);
+            BindingContext = new VMVentiladores(Navigation);
+        }
+	}
+}
